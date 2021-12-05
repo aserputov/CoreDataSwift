ced9a8d1abf69c83c4e5afa1ae4a168ed223bcad
Anatoliy Serputov <aserputov@myseneca.ca>
2021-12-05 03:27:29 -0500

    enhancement: First Row in SqlLite Created.

--- CoreDataPractice/ViewController.swift
+++ CoreDataPractice/ViewController.swift
@@ -16,7 +16,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func addEmployee() {
+        let e1 = Employee(context: self.context)
+        e1.name = "Anatoliy S"
+        e1.dateHired = "01.01.2022"
+        do{
+            try self.context.save()
+            
+        }catch{
+            print("Not ok")
+        }
+        
+    }
 
-
+    @IBAction func GoEmp(_ sender: Any) {
+        addEmployee()
+    }
+    
 }
 
